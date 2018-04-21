393d36e2ffb71dfb4644fea0d40c56fc8c7edf10
Nathaniel Beckstead <becksteadn@gmail.com>
2018-04-20 22:32:54 -0400

    bash arguments are hard

--- Ansible/Linux/keys/make_keys.sh
+++ Ansible/Linux/keys/make_keys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mv id_rsa id_rsa.old
 mv id_rsa.pub id_rsa.pub.old
-ssh-keygen -b 2048 -t rsa -f './$1' -q -N ""
-chmod 400 './$1'
+ssh-keygen -b 2048 -t rsa -f "./$1" -q -N ""
+chmod 400 "./$1"
